1c2f4af8045520a21d0df1a6eb9b17ced3748c19
GiuliaGallorini <giuliagallorini7@gmail.com>
2019-06-27 21:56:34 +0100

    All the routes working. Missing visual part

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const Movie = require("../models/Movie");
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+const router = express.Router();
+
+/* GET home page */ // Route "/" (http://localhost:3000)
+router.get("/", (req, res, next) => {
+  res.render("index"); // Render /views/index.hbs
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+  .then(moviesFromDb => {
+    res.render("movies", { 
+      movies: moviesFromDb 
+    });
+  });
+});
+
+// Example: http://localhost:3000/movies/52cdef7c4bab8bd675297d94
+router.get("/movies/:id", (req, res, next) => {
+  console.log("DEBUG req.params", req.params)
+  let movieId = req.params.id;
+  Movie.findById(movieId).then(movie => {
+    res.render("movie-detail", { movie: movie });
+  });
 });
 
 module.exports = router;
+
